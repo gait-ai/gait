a1b7994d57b51a7023c1cc98400ecb28b9cbd590
Kyle Li <kyleli626@gmail.com>
2024-10-21 12:55:21 -0700

    identify cursor on windows

--- src/ide.ts
+++ src/ide.ts
@@ -3,8 +3,9 @@ export type TOOL = "VSCode" | "Cursor" | "Unknown";
 export function checkTool(): TOOL {
     const bundleIdentifier = process.env.__CFBundleIdentifier || "";
     const ipcHook = process.env.VSCODE_IPC_HOOK || "";
+    const cwd = process.env.VSCODE_CWD || "";
   
-    if (bundleIdentifier.includes("Cursor") || ipcHook.includes("Cursor")) {
+    if (bundleIdentifier.includes("Cursor") || ipcHook.includes("Cursor") || cwd.includes("cursor")) {
       return "Cursor";
     } 
     return "VSCode";
